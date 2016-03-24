184b599a583282e54ce532c8eddd4c84282d29b9
yaojiafeng <18784660@qq.com>
2016-03-24 14:52:23 +0800

    autowire，resource注解

---
+++ common/src/main/java/com/yaojiafeng/common/core/Constants.java
@@ -0,0 +1,13 @@
+/**
+ * qccr.com Inc.
+ * Copyright (c) 2014-2016 All Rights Reserved.
+ */
+package com.yaojiafeng.common.core;
+
+/**
+ * @author yaojiafeng
+ * @since $Revision:1.0.0, $Date: 16/3/23 下午9:03 $
+ */
+public class Constants {
+    public static final String EN = "EN";
+}
--- common/src/test/java/UtilsTest.java
+++ common/src/test/java/UtilsTest.java
@@ -1,14 +1,19 @@
 import com.yaojiafeng.common.util.VelocityUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 /**
  * Created by yaojiafeng on 16/1/31.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring-common.xml"})
+@ContextConfiguration(locations = {"classpath:spring-common.xml", "classpath:spring.xml"})
 public class UtilsTest {
 
 
@@ -16,4 +21,15 @@ public class UtilsTest {
     public void test() {
         System.out.println(VelocityUtils.merge());
     }
+
+    @Resource
+    @Qualifier("languageChangesMap")
+    private Map<String, String> languageChangesMap;
+
+    @Test
+    public void testAutowired() {
+        System.out.println(languageChangesMap.getClass().getSimpleName());
+        System.out.println(languageChangesMap);
+    }
+
 }
