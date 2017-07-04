afb3f5d3e079553f7ffaaf2a7d83ab73a9701155
yaojiafeng <yaojiafeng@cai-inc.com>
2017-07-04 15:32:14 +0800

    使用springcore获取方法参数名称

---
+++ test/src/main/java/com/yaojiafeng/test/asm/ParameterNameTest.java
@@ -0,0 +1,35 @@
+package com.yaojiafeng.test.asm;
+
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.core.ParameterNameDiscoverer;
+
+/**
+ * 根据class文件解析方法参数名
+ *
+ * @author yaojiafeng
+ * @create 2017-07-04 下午2:54
+ * @see http://blog.csdn.net/wwwwenl/article/details/53427039
+ */
+public class ParameterNameTest {
+
+    public static void main(String[] args) {
+        ParameterNameDiscoverer parameterNameDiscoverer =
+                new LocalVariableTableParameterNameDiscoverer();
+        try {
+            String[] parameterNames = parameterNameDiscoverer
+                    .getParameterNames(ParameterNameTest.class.getDeclaredMethod("test",
+                            String.class, int.class));
+            System.out.print("test : ");
+            for (String parameterName : parameterNames) {
+                System.out.print(parameterName + ' ');
+            }
+        } catch (NoSuchMethodException | SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void test(String param1, int param2) {
+        System.out.println(param1 + param2);
+    }
+
+}
