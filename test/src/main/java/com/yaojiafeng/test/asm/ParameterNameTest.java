80642e2965a70b0171c52c715ef10161e3b1365a
四少 <yaojiafeng@cai-inc.com>
2017-12-21 16:38:12 +0800

    LocalVariableTableParameterNameDiscoverer

--- test/src/main/java/com/yaojiafeng/test/asm/ParameterNameTest.java
+++ test/src/main/java/com/yaojiafeng/test/asm/ParameterNameTest.java
@@ -3,9 +3,12 @@ package com.yaojiafeng.test.asm;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.core.ParameterNameDiscoverer;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
 /**
  * 根据class文件解析方法参数名
- *
+ * <p>
  * 解析的方法不能是接口的方法，因为接口方法字节码不保存参数名称
  *
  * @author yaojiafeng
@@ -15,6 +18,15 @@ import org.springframework.core.ParameterNameDiscoverer;
 public class ParameterNameTest {
 
     public static void main(String[] args) {
+        spring();
+        jdk();
+    }
+
+    public static void test(String param1, int param2) {
+        System.out.println(param1 + param2);
+    }
+
+    public static void spring() {
         ParameterNameDiscoverer parameterNameDiscoverer =
                 new LocalVariableTableParameterNameDiscoverer();
         try {
@@ -30,8 +42,21 @@ public class ParameterNameTest {
         }
     }
 
-    public static void test(String param1, int param2) {
-        System.out.println(param1 + param2);
+    /**
+     * java8需设置编译参数-parameters
+     */
+    public static void jdk() {
+        System.out.println();
+        Method method = null;
+        try {
+            method = ParameterNameTest.class.getDeclaredMethod("test",
+                    String.class, int.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        for (Parameter p : method.getParameters()) {
+            System.out.println("parameter: " + p.getType().getName() + ", " + p.getName());
+        }
     }
 
 }
