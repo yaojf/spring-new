0a6eb3b716c509111e4b12be945dd981a20c02b5
yaojiafeng <18784660@qq.com>
2018-08-07 10:36:00 +0800

    QLExpress测试代码提交

---
+++ test/src/main/java/com/yaojiafeng/test/QLExpress/QLExpressTest.java
@@ -0,0 +1,77 @@
+package com.yaojiafeng.test.QLExpress;
+
+import com.ql.util.express.*;
+import com.ql.util.express.instruction.detail.Instruction;
+import com.ql.util.express.instruction.detail.InstructionCallSelfDefineFunction;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * 自定义配置函数解析调用
+ *
+ * Created by yaojiafeng on 2018/8/6 下午8:14.
+ */
+public class QLExpressTest {
+
+    public static void main(String[] args) throws Exception {
+
+        DefaultContext<String, Object> context = new DefaultContext<String, Object>();
+        ExpressRunner runner = new ExpressRunner();
+
+        List<String> fun = new ArrayList<>();
+
+        InstructionSet instructionSet = runner.parseInstructionSet("shopAreaIn(19)&&itemAreaIn(19)");
+        if (instructionSet != null && instructionSet.getCurrentPoint() > 0) {
+            for (int i = 0; i <= instructionSet.getCurrentPoint(); i++) {
+                Instruction instruction = instructionSet.getInstruction(i);
+                if (instruction.getClass().getName().indexOf("InstructionCallSelfDefineFunction") > 0) {
+                    InstructionCallSelfDefineFunction instructionFunction = (InstructionCallSelfDefineFunction) instruction;
+                    context.put("shopAreaList", Arrays.asList("19"));
+                    fun.add(instructionFunction.getFunctionName());
+                }
+            }
+        }
+
+        runner.addFunction("shopAreaIn", new ContainShopAreaHandle());
+        runner.addFunction("itemAreaIn", new ContainShopAreaHandle());
+
+        Boolean r = (Boolean) runner.execute("shopAreaIn(19)&&itemAreaIn(19)", context, null, false, true);
+        System.out.println(r);
+    }
+
+
+    public static class ContainShopAreaHandle extends Operator {
+
+        @Override
+        public OperateData executeInner(InstructionSetContext context, ArraySwap arraySwap) throws Exception {
+            Object parameter = context.get("shopAreaList");
+            List<String> parameterList = (List<String>) parameter;
+            Boolean returnValue = containList(arraySwap, context, parameterList);
+            return new OperateData(returnValue, returnValue.getClass());
+        }
+
+        public Boolean containList(ArraySwap arraySwap, InstructionSetContext context, List<String> values) throws Exception {
+            if (values == null || values.size() == 0) {
+                return Boolean.FALSE;
+            } else {
+                for (int i = 0; i < arraySwap.length; i++) {
+                    String data = arraySwap.get(i).getObject(context).toString();
+                    for (String value : values) {
+                        if (value.trim().equals(data.trim())) {
+                            return Boolean.TRUE;
+                        }
+                    }
+                }
+            }
+            return Boolean.FALSE;
+        }
+
+        @Override
+        public Object executeInner(Object[] objects) throws Exception {
+            return null;
+        }
+    }
+}
