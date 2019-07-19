bb062af21e0208220940318e0730be36974f1fff
yaojiafeng <18784660@qq.com>
2019-07-19 16:35:33 +0800

    hession2序列化测试

--- common/src/main/java/com/yaojiafeng/common/hessian/Hessians.java
+++ common/src/main/java/com/yaojiafeng/common/hessian/Hessians.java
@@ -4,6 +4,9 @@ import com.caucho.hessian.io.Hessian2Input;
 import com.caucho.hessian.io.Hessian2Output;
 import com.caucho.hessian.io.HessianInput;
 import com.caucho.hessian.io.HessianOutput;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -11,9 +14,10 @@ import java.io.IOException;
 
 /**
  * hession序列化目前来看是针对field字段来进行序列化和反序列化，跟set get方法无关
+ * <p>
+ * 反序列化的对象增减字段不会影响反序列化结果，新增的字段为null，减少的字段忽略，不会报异常，而且不会校验serialVersionUID
  *
- * 反序列化的对象增减字段不会影响反序列化结果，新增的字段为null，减少的字段忽略
- *
+ * 序列化方式必须配对使用，不同序列化反序列化方式会报异常（比如用Hessian序列化，用Hessian2反序列化会报异常，反之亦然）
  * @author yaojiafeng
  * @create 2017-06-29 下午3:48
  */
@@ -25,6 +29,7 @@ public class Hessians {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         Hessian2Output ho = new Hessian2Output(os);
         ho.writeObject(obj);
+        ho.flush();
         return os.toByteArray();
     }
 
@@ -36,17 +41,18 @@ public class Hessians {
         return hi.readObject();
     }
 
-    public static byte[] serialize(Object obj) throws IOException{
-        if(obj==null) throw new NullPointerException();
+    public static byte[] serialize(Object obj) throws IOException {
+        if (obj == null) throw new NullPointerException();
 
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         HessianOutput ho = new HessianOutput(os);
         ho.writeObject(obj);
+        ho.flush();
         return os.toByteArray();
     }
 
-    public static Object deserialize(byte[] by) throws IOException{
-        if(by==null) throw new NullPointerException();
+    public static Object deserialize(byte[] by) throws IOException {
+        if (by == null) throw new NullPointerException();
 
         ByteArrayInputStream is = new ByteArrayInputStream(by);
         HessianInput hi = new HessianInput(is);
@@ -57,48 +63,31 @@ public class Hessians {
 
         City city = new City(1L, "beijing");
 
+        // hessian
         byte[] bytes = serialize(city);
 
         Object o = deserialize(bytes);
 
         System.out.println(o);
+
+
+        // hessian2
+        bytes = serialize2(city);
+
+        o = deserialize2(bytes);
+
+        System.out.println(o);
     }
 
-
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
     private static class City implements java.io.Serializable {
 
         private Long id;
 
         private String cityName;
 
-        public City(Long id, String cityName) {
-            this.id = id;
-            this.cityName = cityName;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getCityName() {
-            return cityName;
-        }
-
-        public void setCityName(String cityName) {
-            this.cityName = cityName;
-        }
-
-        @Override
-        public String toString() {
-            return "City{" +
-                    "id=" + id +
-                    ", cityName='" + cityName + '\'' +
-                    '}';
-        }
     }
 
 }
